9c146382cdc7b563f9c4fba912cd5dd472d62d77
nehachougule1998 <nchougule734@gmail.com>
2022-08-16 23:50:30 +0530

    Validation

--- HelloWorldBridgelabz/src/app/app.component.ts
+++ HelloWorldBridgelabz/src/app/app.component.ts
@@ -6,17 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "HelloWorldBridgelabz";
-  imgUrl="../assets/BL.jpeg";
+  title = 'HelloWorldBridgelabz';
+  imgUrl = "../assets/BL.jpeg";
   url = "https://www.bridgelabz.com";
-  userName: String = "";
+  userName: string="";
+  nameError: string= "";
 
   ngOnInit(): void{
     this.title="Hello From Bridgelabz";
   }
+  
   onClick($event: any){
-  console.log("Save button is clicked!", $event);
-  window.open(this.url, "_blank");
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+  onInput($event:any){
+    console.log("change Event Occured", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
+  }
 }
-}
-
